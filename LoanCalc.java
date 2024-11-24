5a332b63a91aa54cb5a14d4758e9613b6c7e837b
Idan Mor <idan.mor@post.runi.ac.il>
2024-11-24 13:59:45 +0200

    reverted debug changes

--- LoanCalc.java
+++ LoanCalc.java
@@ -9,9 +9,9 @@ public class LoanCalc {
     // interest rate (double, as a percentage), and number of payments (int).  
 	public static void main(String[] args) {		
 		// Gets the loan data
-		double loan = 100000.0;
-		double rate = 5.0;
-		int n = 10;
+		double loan = Double.parseDouble(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
 		// Computes the periodical payment using brute force search
